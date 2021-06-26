0229bae5fe57501c1fee2a0f08a14761f6805354
anilakheli <kanxoakheli22@gmail.com>
2021-06-26 23:45:40 +0545

    adding add function on pressrelease

--- client/src/sagas/pressrelease.js
+++ client/src/sagas/pressrelease.js
@@ -33,3 +33,49 @@ export function* fetchpressreleaseRequest(api, action) {
       yield put(PressReleaseActions.fetchpressreleaseFailure());
     }
   }
+
+  //----------------- addpressrelease
+  export function* addpressreleaseRequest(api, action) {
+    const { payload, attachment } = action;
+  
+    const pressrelease = payload.pressrelease.data;
+    //   if (!isNil(attachment)) {
+    //     const _payload = {
+    //       attachment_hash: null,
+    //       file: attachment,
+    //     };
+    //     const res = yield api.getS3Info();
+  
+    //     if (res.ok) {
+    //       // eslint-disable-next-line no-undef
+    //       const formData = new FormData();
+    //       for (const key in res.data.inputs) {
+    //         formData.append(key, res.data.inputs[key]);
+    //       }
+    //       formData.set("Content-Type", _payload.file.type);
+    //       formData.append(
+    //         "key",
+    //         `attachments/${_payload.attachment_hash}/${_payload.file.name}`
+    //       );
+    //       formData.append("file", _payload.file);
+    //       const response = yield api.fileUpload(formData);
+  
+    //       if (response.ok) {
+    //         const data = xmljs.xml2json(response.data, {
+    //           compact: true,
+    //           ignoreDeclaration: true,
+    //         });
+    //         const url = JSON.parse(data).PostResponse.Location._text;
+    //         payload.document.data.attachment = url;
+    //       }
+    //     }
+    //   }
+  
+    const response = yield api.postPressReleaseSave(pressrelease);
+  
+    if (response.ok) {
+      yield put(PressReleaseActions.addpressreleaseSuccess());
+    } else {
+      yield put(PressReleaseActions.addpressreleaseFailure());
+    }
+  }
