c8ef51514622895b54b83ac9f24d3b6db2c216f1
anilakheli <kanxoakheli22@gmail.com>
2021-06-26 22:42:02 +0545

    adding fetch function on pressrelease

--- client/src/reducers/pressRelease.js
+++ client/src/reducers/pressRelease.js
@@ -14,22 +14,41 @@ const fetchallpressreleaseSuccess = (state, action) => {
   return state.merge({
     ...state,
     status: "done",
-    allbooksData: action.response,
+    allpressreleaseData: action.response,
   });
 };
 const fetchallpressreleaseFailure = (state, action) => {
     state.merge({ ...state, status: "error" });
   };
 
+  //fetch a pressrelease
+const fetchpressreleaseRequest = (state, action) =>
+state.merge({ ...state, token: "", status: "pending" });
+const fetchpressreleaseSuccess = (state, action) => {
+return state.merge({
+  ...state,
+  status: "done",
+  pressreleaseData: action.response,
+});
+};
+const fetchpressreleaseFailure = (state, action) => {
+state.merge({ ...state, status: "error" });
+};
+
 const clearRequest = (state, action) =>
 state.merge({ ...state, ...initialState });
 
 export const reducer = createReducer(initialState, {
 
- //Fetch allbooks   
+ //Fetch allpress release
 [PressReleaseTypes.FETCHALLPRESSRELEASE_REQUEST]: fetchallpressreleaseRequest,
 [PressReleaseTypes.FETCHALLPRESSRELEASE_SUCCESS]: fetchallpressreleaseSuccess,
 [PressReleaseTypes.FETCHALLPRESSRELEASE_FAILURE]: fetchallpressreleaseFailure,
+
+//fetch press release
+[PressReleaseTypes.FETCHPRESSRELEASE_REQUEST]: fetchpressreleaseRequest,
+[PressReleaseTypes.FETCHPRESSRELEASE_SUCCESS]: fetchpressreleaseSuccess,
+[PressReleaseTypes.FETCHPRESSRELEASE_FAILURE]: fetchpressreleaseFailure,
 
 [PressReleaseTypes.CLEAR_REQUEST]: clearRequest,
 });
