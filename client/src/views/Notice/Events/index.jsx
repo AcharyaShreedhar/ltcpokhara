68509c5aa22dd79b831e1cb4dc21837a3c3c2b88
Shreedhar Acharya <spacharya2537@gmail.com>
2021-06-26 10:36:04 +0545

    adding pagination

--- client/src/views/Notice/Events/index.jsx
+++ client/src/views/Notice/Events/index.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
-import {isNil} from "ramda"
+import { isNil } from "ramda";
 import { connect } from "react-redux";
 import { Table } from "react-bootstrap";
 import { englishToNepaliNumber } from "nepali-number";
+import ReactPaginate from "react-paginate";
+import AdminActions from "../../../actions/admin";
 
 const headings = ["शीर्षक", "रुजुकर्ता", "मिति", ""];
 
 class Events extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { perPage: 10, page: 1 };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    var eventsList = [];
+    if (nextProps != prevState) {
+      eventsList = nextProps.eventsData;
+    }
+    const pageCount = !isNil(eventsList)
+      ? Math.ceil(eventsList.total / nextProps.perPage)
+      : 10;
+
+    const data = !isNil(eventsList) ? eventsList.list : [];
+
+    return { data, pageCount };
+  }
+
+  handlePageChange(data) {
+    const { perPage } = this.state;
+    this.setState({ page: data.selected });
+
+    this.props.fetchallEvents({
+      name: "events_date",
+      page: data.selected * perPage,
+      perPage,
+    });
+  }
+
   render() {
-    
-    const eventList = isNil(this.props.eventsData)?[]:this.props.eventsData
+    const { data, pageCount } = this.state;
     return (
       <div className="content">
         <div className="titlebar">कार्यक्रमहरू</div>
@@ -24,7 +56,7 @@ class Events extends Component {
               </tr>
             </thead>
             <tbody>
-              {eventList.map((event, index) => (
+              {data.map((event, index) => (
                 <tr>
                   <td>{englishToNepaliNumber(index + 1)}</td>
                   <td key={index}> {event.notice_title}</td>
@@ -35,6 +67,20 @@ class Events extends Component {
               ))}
             </tbody>
           </Table>
+          <div className="paginationStyle">
+            <ReactPaginate
+              previousLabel={"PREV"}
+              nextLabel={"NEXT"}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={this.handlePageChange}
+              containerClassName={"pagination"}
+              activeClassName={"active"}
+            />
+          </div>
         </div>
       </div>
     );
@@ -44,5 +90,9 @@ class Events extends Component {
 const mapStateToProps = (state) => ({
   eventsData: state.admin.programmes,
 });
+const mapDispatchToProps = (dispatch) => ({
+  fetchallEvents: (payload) =>
+    dispatch(AdminActions.fetchalleventsRequest(payload)),
+});
 
-export default connect(mapStateToProps, null)(Events);
+export default connect(mapStateToProps, mapDispatchToProps)(Events);
