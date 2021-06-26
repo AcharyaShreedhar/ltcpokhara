e453bf0dce3c4a2143040a330ada5eb8eb35d175
Ashish kc <kecyasis21@gmail.com>
2021-06-26 17:48:05 +0545

    code added for booklist in src/sagas/publication.js

---
+++ client/src/sagas/publication.js
@@ -0,0 +1,18 @@
+import { call, put } from "redux-saga/effects";
+import { toast } from "react-toastify";
+
+import { history } from "../reducers";
+import { isNil } from "ramda";
+import PublicationActions from "../actions/publication";
+
+export function* fetchallbooksRequest(api, action) {
+  const { payload } = action;
+  const payloaddata = isNil(payload) ? action : payload;
+  const response = yield api.getBooksList(payloaddata);
+
+  if (response.ok) {
+    yield put(PublicationActions.fetchallbooksSuccess(response.data));
+  } else {
+    yield put(PublicationActions.fetchallbooksFailure());
+  }
+}
