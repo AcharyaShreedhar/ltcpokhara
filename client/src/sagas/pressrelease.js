b822396d565da5f5708586770960827505054280
anilakheli <kanxoakheli22@gmail.com>
2021-06-26 22:11:06 +0545

    adding fetchall function on pressrelease

---
+++ client/src/sagas/pressrelease.js
@@ -0,0 +1,22 @@
+import { call, put } from "redux-saga/effects";
+import { toast } from "react-toastify";
+
+import { history } from "../reducers";
+import { isNil } from "ramda";
+import PressReleaseActions from "../actions/pressRelease";
+import pressRelease from "../views/Notice/PressRelease";
+
+//----------------fetchallpressrelease
+export function* fetchallpressreleaseRequest(api, action) {
+  const { payload } = action;
+  const payloaddata = isNil(payload) ? action : payload;
+  const response = yield api.getPressReleaseList(payloaddata);
+
+  if (response.ok) {
+    yield put(PressReleaseActions.fetchallpressreleaseSuccess(response.data));
+  } else {
+    yield put(PressReleaseActions.fetchallpressreleaseFailure());
+  }
+}
+
+
