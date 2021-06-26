853459a955ac3ba205d532c5ff6da0902845c840
anilakheli <kanxoakheli22@gmail.com>
2021-06-26 22:09:48 +0545

    adding fetchall function on press release

---
+++ client/src/actions/pressRelease.js
@@ -0,0 +1,20 @@
+import { createActions } from "reduxsauce";
+
+const { Types, Creators } = createActions({
+  /*
+   *** Please add redux functions which should do API calls here.
+   */
+
+ 
+
+  // fetching all pressrelease
+  fetchallpressreleaseRequest: ["payload"],
+  fetchallpressreleaseSuccess: ["response"],
+  fetchallpressreleaseFailure: null,
+
+    // Clearn all caches
+    clearRequest: null,
+});
+
+export const PressReleaseTypes = Types;
+export default Creators;
